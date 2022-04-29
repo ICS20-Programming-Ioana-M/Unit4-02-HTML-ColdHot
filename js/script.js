e3ca02eab50c6c79380d89dc37c1206dbb540040
Ioana <99274396+Ioana-Mar@users.noreply.github.com>
2022-04-29 14:38:40 +0000

    js doesn't work but that's okay

---
+++ js/script.js
@@ -0,0 +1,21 @@
+'use strict'
+
+// function to indicate wether the temperature is cold or hot
+function buttonClicked(){
+  // variables for user's temperature input
+  let temp = parseInt(document.getElementById("temp").value)
+   
+  // if statement that checks if the temperature is hot, cold, or just right
+  if(temp < 0){
+    // tells user it's cold
+    document.getElementById('answer').innerHTML = "It's cold. >:("
+    
+  } else if (temp > 10){
+    // tells user it's hot
+    document.getElementById('answer').innerHTML = "It's hot. :3"
+    
+  } else {
+    // tells user it's the perfect temperature
+    document.getElementById('answer').innerHTML = "It's the perfect temperature. :D"
+  }
+}
